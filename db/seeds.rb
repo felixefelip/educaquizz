cf72a3f2df4bf9d4dff61662a481291a97324e8f
Felipe Felix <felippe.felix98@gmail.com>
2023-05-07 21:29:15 -0300

    feat: questions seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+if (Quiz.count.zero?)
+  ApplicationRecord.transaction do
+    quiz1 = Quiz.create!(description: "História")
+
+    Question.create!(description: "Qual o nome do presidente do Brasil que ficou conhecido como Jango",
+                     difficulty: 3,
+                     quiz_id: quiz1.id,
+                     alternative1: "Jânio Quadros",
+                     alternative2: "Jacinto Anjos",
+                     alternative3: "Getúlio Vargas",
+                     alternative4: "João Goulart",
+                     correct_answer: 3)
+
+    Question.create!(description: "Em que ano foi descoberto o Brasil?",
+                     difficulty: 2,
+                     quiz_id: quiz1.id,
+                     alternative1: 1891,
+                     alternative2: 2010,
+                     alternative3: 1520,
+                     alternative4: 1500,
+                     correct_answer: 4)
+  end
+end
