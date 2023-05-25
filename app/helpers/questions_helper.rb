1cbd5413df6d3114510715d61df684c5fef51fce
Felipe Felix <felippe.felix98@gmail.com>
2023-05-24 23:26:31 -0300

    feat: poder escolher todas as alternativas

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,11 @@
 module QuestionsHelper
+  def link_to_respond_question(text:, quiz_realization_answer:)
+    link_to(text, question_quiz_realization_quiz_realization_answers_url(
+              answer: 1,
+              question_id: quiz_realization_answer.question.id,
+              quiz_realization_id: quiz_realization_answer.quiz_realization_id,
+            ),
+            method: :post,
+            data: { confirm: "Tem certeza?", "turbo-method": :post })
+  end
 end
