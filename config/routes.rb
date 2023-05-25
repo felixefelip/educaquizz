678e164f7f52b9657445ea97f304f5706eb12547
Felipe Felix <felippe.felix98@gmail.com>
2023-05-25 00:03:02 -0300

    refactor: remover comentarios das rotas

--- config/routes.rb
+++ config/routes.rb
@@ -18,9 +18,4 @@ Rails.application.routes.draw do
       resources :quiz_realization_answers, only: %i[new create]
     end
   end
-
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
