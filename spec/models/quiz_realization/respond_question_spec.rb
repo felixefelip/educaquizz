b260c7b07861ef73e8df7c9452cc9119f6cbfc87
Felipe Felix <felipe.felix@maino.com.br>
2023-06-10 20:47:02 -0300

    test: checar se criou quiz realization answer

--- spec/models/quiz_realization/respond_question_spec.rb
+++ spec/models/quiz_realization/respond_question_spec.rb
@@ -10,7 +10,9 @@ RSpec.describe QuizRealization::RespondQuestion do
         teacher_quiz_realization = create(:teacher_quiz_realization, quiz:)
         quiz_realization = create(:quiz_realization, teacher_quiz_realization:)
 
-        result = described_class.call(answer: 2, question: question1, quiz_realization:)
+        result = nil
+        expect { result = described_class.call(answer: 2, question: question1, quiz_realization:) }
+          .to change(QuizRealizationAnswer, :count).by(1)
 
         expect(result).to be_success
         expect(result.type).to eq(:there_are_more_questions)
@@ -29,7 +31,9 @@ RSpec.describe QuizRealization::RespondQuestion do
         expect(result1).to be_success
         expect(result1.type).to eq(:there_are_more_questions)
 
-        result2 = described_class.call(answer: 3, question: question2, quiz_realization:)
+        result2 = nil
+        expect { result2 = described_class.call(answer: 2, question: question2, quiz_realization:) }
+          .to change(QuizRealizationAnswer, :count).by(1)
 
         expect(result2).to be_success
         expect(result2.type).to eq(:last_question_answered)
