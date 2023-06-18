65a98944b0dab40e5c301c119626e1d0916d9d64
Felipe Felix <felipe.felix@maino.com.br>
2023-06-18 13:09:31 -0300

    feat/fix: formatar data do i18n

--- config/application.rb
+++ config/application.rb
@@ -19,9 +19,9 @@ module Educaquizz
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
-    I18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}').to_s]
-    config.i18n.default_locale = :'pt-BR'
+    I18n.load_path += Dir[Rails.root.join("config/locales/*.{rb,yml}'")]
+    config.i18n.default_locale = "pt-BR"
 
-    config.time_zone = 'Brasilia'
+    config.time_zone = "Brasilia"
   end
 end
