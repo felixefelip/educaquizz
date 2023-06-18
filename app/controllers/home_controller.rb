05d1e4954743123981491e3014872e1ddad7bf6e
Felipe Felix <felipe.felix@maino.com.br>
2023-06-18 20:14:22 -0300

    feat: form do rails no cadastro

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   skip_before_action :authenticate_user!
 
-  def index; end
+  def index
+    redirect_to quizzes_path if current_user.present?
+  end
 end
